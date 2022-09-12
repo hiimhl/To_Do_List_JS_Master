719686b81f1e9dfcac5024fecbcbf92f45beff3d
hlin <gywjd5126@gmail.com>
2022-09-12 13:23:24 +0900

    To Do List - setup

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { DefaultTheme } from "styled-components";
+// theme 타입을 정의한 파일.
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#cbf3f0",
+  textColor: "#051923",
+  accentColor: "#7678ed",
+};
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#272643",
+  textColor: "white",
+  accentColor: "#d9ed92",
+};
