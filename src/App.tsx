5db965e0bc1b9113c4dd0b53269a060e6da26715
hlin <gywjd5126@gmail.com>
2022-09-24 21:37:47 +0900

    Set Selectors - Selector로 값 수정하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { hourSelector, minuteState } from "./atoms";
 
-// GlobalStye = 렌더링 될때 스타일 태그에 적용되는 컴포넌트, 모든 컴포넌트에 적용됨
-
 function App() {
   const [minutes, setMinutes] = useRecoilState(minuteState);
-  const hours = useRecoilValue(hourSelector);
+  const [hours, setHours] = useRecoilState(hourSelector);
 
   const onMinutesChange = (event: React.FormEvent<HTMLInputElement>) => {
     setMinutes(+event.currentTarget.value);
+  };
+  const onHoursChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
   };
 
   console.log(minutes);
@@ -21,7 +22,12 @@ function App() {
         type="number"
         placeholder="Minutes"
       />
-      <input value={hours} type="number" placeholder="Hours" />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
     </div>
   );
 }
